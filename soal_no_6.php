87d5c32d5bf40a2f5abd4ad480ed35fee75db41e
ikipazil1803 <50256010+ikipazil1803@users.noreply.github.com>
2019-05-04 09:28:34 -0400

    Update soal_no_6.php

--- soal_no_6.php
+++ soal_no_6.php
@@ -1,10 +1,8 @@
 <?php
 $db = mysqli_connect("localhost", "root", "", "homestead");
-
 if (isset($_POST['id'])) {
     $id = $_POST['id'];
     $sql = "UPDATE candidates SET earned_vote = earned_vote + 1 WHERE id=$id";
-
     if (mysqli_query($db, $sql)=== true) {
         echo json_encode(array('success' => true));
         exit;
@@ -15,11 +13,9 @@ if (isset($_POST['id'])) {
     
     mysqli_close($db);
 }
-
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
     $sql = "SELECT * FROM `candidates` ORDER BY `candidates`.`earned_vote` DESC";
     $result = mysqli_query($db, $sql);
     
@@ -30,10 +26,7 @@ if (mysqli_connect_errno()) {
     } else {
         $data = [];
     }
-
     mysqli_close($db);
-
-
 ?>
 <html lang="en">
 
@@ -41,24 +34,72 @@ if (mysqli_connect_errno()) {
         <meta charset="utf-8">
         <title>jQuery.post demo</title>
         <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <style>
             tr td:last-child {
                 width: 200px;
                 text-align: center;
             }
+            table{
+                padding-top:30px;
+                margin-top:190px;
+            }
+            @import url(https://fonts.googleapis.com/css?family=Anonymous+Pro);
+
+/* Global */
+html{
+  min-height: 100%;
+  overflow: hidden;
+}
+body{
+  height: calc(100vh - 8em);
+  padding: 4em;
+  color: rgba(255,255,255,.75);
+  font-family: 'Anonymous Pro', monospace;  
+  background-color: rgb(25,25,25);  
+}
+.line-1{
+    position: relative;
+    top: 50%;  
+    width: 24em;
+    margin: 0 auto;
+    border-right: 2px solid rgba(255,255,255,.75);
+    font-size: 180%;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    transform: translateY(-50%);    
+}
+
+/* Animation */
+.anim-typewriter{
+  animation: typewriter 4s steps(44) 1s 1 normal both,
+             blinkTextCursor 500ms steps(44) infinite normal;
+}
+@keyframes typewriter{
+  from{width: 0;}
+  to{width: 24em;}
+}
+@keyframes blinkTextCursor{
+  from{border-right-color: rgba(255,255,255,.75);}
+  to{border-right-color: transparent;}
+}
+
         </style>
     </head>
 
-    <body>
+    <body style="background-image:url('https://cdn-images-1.medium.com/max/1600/1*OcODYT4IhrThbJLwcLOSPg.jpeg');">
+    <p class="line-1 anim-typewriter">My name Is muhammad Zulkifli</p>
 
-        <table border="1">
+
+         <table class="table table-dark"  border="1">
             <?php
                 foreach ($data as $k) {
                     echo "
                     <tr id=".$k["id"].">
                         <td>".$k["name"]."</td>
-                        <td rowspan='2'><button type='submit'>+</button></td>
+                        <td rowspan='2'><button type='submit'><i class='material-icons'>add</i></button></td>
                     </tr>
                     <tr>
                         <td id='suara".$k["id"]."'>Perolehan suara: ".$k["earned_vote"]."</td>
@@ -72,11 +113,8 @@ if (mysqli_connect_errno()) {
         <script>
             $("button").click(function(e) {
                 e.preventDefault();
-
                 // Get some values from elements on the page:
                 var id = $(this).closest('tr').attr('id');
-
-
                 $.ajax({
                         method: "POST",
                         url: 'suara.php',
